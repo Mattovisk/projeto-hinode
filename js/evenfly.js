f5f74b9675ea286ecaa3c4afb86611049fcca9f0
mattovisk <matheus.oliv@live.com>
2017-02-06 20:44:38 -0200

    eoq

--- js/evenfly.js
+++ js/evenfly.js
@@ -31,3 +31,62 @@ $(function () {
         }
     });
 });
+
+
+$(document).ready(function () {
+    $("#phone").mask("(99) 9999-9999?9");
+
+
+    $("#phone").on("blur", function () {
+        var last = $(this).val().substr($(this).val().indexOf("-") + 1);
+
+        if (last.length == 5) {
+            var move = $(this).val().substr($(this).val().indexOf("-") + 1, 1);
+
+            var lastfour = last.substr(1, 4);
+
+            var first = $(this).val().substr(0, 9);
+
+            $(this).val(first + move + '-' + lastfour);
+        }
+    });
+
+    $("#contato").submit(function (e) {
+
+        //prevent Default functionality
+        e.preventDefault();
+
+        //get the action-url of the form
+        var actionurl = e.currentTarget.action;
+
+        var conteudo = $("#contato").serializeArray().map(function (data) {
+            return data.name = data.value
+        });
+        //do your own request an handle the results
+        $.ajax({
+            url: actionurl,
+            type: 'post',
+            dataType: 'json',
+            data: JSON.stringify(conteudo),
+            success: function (data) {
+                console.log('teste');
+                // ... do something with the data...
+            }
+        });
+
+    });
+
+    // $('#enviar').click(function () {
+    //     $.ajax({
+    //         url: 'http://api.intuitivalab.com/ph_email',
+    //         type: 'post',
+    //         dataType: 'json',
+    //         data: $('form#contato').serialize(),
+    //         success: function(data) {
+    //             console.log('success');
+    //                //$('form#contato').hide;
+    //              }
+    //     });
+    // });
+
+});
